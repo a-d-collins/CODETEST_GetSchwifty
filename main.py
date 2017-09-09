7fd6e95ded1b20f58b920e8376c24ad8a87f9e6d
a-d-collins <adcollins91@gmail.com>
2017-09-08 20:42:57 -0400

    Start logic for optimal_drone_delivery method

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from httplib import *
+from geopy.distance import vincenty
+import copy
+import datetime
+
+from utils.bisect_helpers import BisectHelpers
+
+
+WAREHOUSE_LAT = -37.816359
+WAREHOUSE_LNG = 144.963848
+DRONE_SPEED = 50 # km / h
+START_TIME = datetime.datetime.now()
+
 
 def deliver_packages():
     """
@@ -10,7 +22,7 @@ def deliver_packages():
     5) If we want to get a package to its destination the soonest:
         a) assign (if possible) the first drone to the first package
         b) take the drone and package off of the lists/queues and
-        c) add drone/package pair to "assignements" list.
+        c) add drone/package pair to "assignments" list.
         - If not possible to deliver package, put package in "unassigned" list.
     6) If, instead, we want to maximize the number of packages delivered, we can do one of at least two things:
         a) Pick lowest ranked drone that can possibly deliver a package, do this for all packages until we have the maximum
@@ -29,20 +41,44 @@ def deliver_packages():
 def update_drones_and_packages(drones, packages):
     # get time it takes for all drones to return to the warehouse
     drones = update_drone_data(drones)
-
     # sort drones by time it takes to get to warehouse
-    # TODO
+    drones = sorted(drones, key=lambda x: x["hours_to_warehouse"])
 
     # update packages with time it takes for them to get to their destinations
     packages = update_package_data(packages)
-
     # sort packages by total time available - time to destination from warehouse
-    # TODO
+    packages = sorted(packages, key=lambda x: x["dest_hours_from_warehouse"])
 
     return [drones, packages]
 
 
 def optimal_drone_delivery(drones, packages):
+    bisect_helpers = BisectHelpers()
+
+    result = {
+        "assignments": [],
+        "unassignedPackageIds": []
+    }
+
+    tmp_drones = copy.deepcopy(drones)
+    used_drones = []
+    for package in packages:
+        # Find rightmost index of drone that can deliver the package
+        # This drone represents the drone that can just barely get the package to its destination on time
+        deadline = datetime.datetime.fromtimestamp(package["deadline"])
+        datetime_diff = deadline - datetime.timedelta(hours=package["dest_hours_from_warehouse"]) - START_TIME
+        hour_limit = datetime_diff.total_seconds() / 3600
+        idx = bisect_helpers.find_lt_by_key(tmp_drones, hour_limit, "hours_to_warehouse")
+
+        if idx is None:
+            result["unassignedPackageIds"].append(package["packageId"])
+        else:
+            result["assignments"].append({"droneId": tmp_drones[idx]["droneId"], "packageId": package["packageId"]})
+            used_drones.append(tmp_drones[idx])
+            tmp_drones.pop(idx)
+
+    # TODO: replace drones with ones that are faster
+
     return {}
 
 
@@ -68,8 +104,30 @@ def get_data_from_api(host, method, url):
 
 
 def update_drone_data(drones):
-    pass
+    for drone in drones:
+        if len(drone["packages"]) > 0:
+            drone_dist_to_warehouse = vincenty(
+                (drone["location"]["latitude"], drone["location"]["longitude"]),
+                (drone["packages"][0]["destination"]["latitude"], drone["packages"][0]["destination"]["longitude"])
+            ).kilometers
+
+            drone_dist_to_warehouse += vincenty(
+                (drone["packages"][0]["destination"]["latitude"], drone["packages"][0]["destination"]["longitude"]),
+                (WAREHOUSE_LAT, WAREHOUSE_LNG)
+            ).kilometers
+        else:
+            drone_dist_to_warehouse = vincenty(
+                (drone["location"]["latitude"], drone["location"]["longitude"]),
+                (WAREHOUSE_LAT, WAREHOUSE_LNG)
+            ).kilometers
+
+        drone["hours_to_warehouse"] = drone_dist_to_warehouse / DRONE_SPEED
 
 
 def update_package_data(packages):
-    pass
+    for package in packages:
+        dest_dist_from_warehouse = vincenty(
+            (WAREHOUSE_LAT, WAREHOUSE_LNG),
+            (package["destination"]["latitude"], package["destination"]["longitude"])
+        ).kilometers
+        package["dest_hours_from_warehouse"] = dest_dist_from_warehouse / DRONE_SPEED
